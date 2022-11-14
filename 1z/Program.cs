e38ca103268c9a00efdd836ab11c4b1887b8d846
MariannaSarkisian <spn.mjj@gmail.com>
2022-11-14 14:47:47 +0300

    1z

---
+++ 1z/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void numbers(int M, int N)
+{
+  if (M<=N)
+    {
+    Console.Write($"{M} ");
+    numbers(M+1, N);
+    }
+}
+Console.Write("Введите первое число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if(M>0)
+{
+    if (N>0) 
+     numbers(M,N);
+}
